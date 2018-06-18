78334f6927a23daa8f329c44a3760d623cdf0a3d
Vitalie Lazu <vitalie.lazu@gmail.com>
2018-06-18 16:26:20 +0300

    Function based views

---
+++ polls/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Question, Choice
+
+def index(req):
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  context = {'latest_question_list': latest_question_list}
+
+  return render(req, 'polls/index.html', context)
+
+def detail(req, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  return render(req, 'polls/detail.html', {'question': question})
+
+def results(req, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  return render(req, 'polls/results.html', {'question': question})
+
+def vote(req, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=req.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    # Redisplay the question voting form
+    return render(req, 'polls/detail.html',{
+      'question': question,
+      'error_message': 'You did not select a choice.',
+    })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
