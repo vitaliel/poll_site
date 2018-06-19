1cc0386b7b2eb3ab5f49f9d5a3052f16dcad1ca6
Vitalie Lazu <vitalie.lazu@gmail.com>
2018-06-19 14:29:23 +0300

    Admin area tweaks

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+  extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+  search_fields = ['question_text']
+  list_filter = ['pub_date']
+  list_display = ('question_text', 'pub_date', 'was_published_recently')
+  # fields = ['pub_date', 'question_text']
+  fieldsets = [
+    (None, {'fields':['question_text']}),
+    ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+  ]
+  inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
